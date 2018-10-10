92af97b850c80c3733983fd2a7f57074e9816775
Hryhorii Furletov <littlefuntik@gmail.com>
2018-10-10 12:57:09 +0300

    Add "env" config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3000,
   openApi: {
